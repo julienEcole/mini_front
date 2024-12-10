7fb66e6d4b13637753201dc1cf46e39bc70dd031
julienEcole <julien.riviere94120@gmail.com>
2024-12-10 21:00:09 +0100

    feat : implementation repository

---
+++ lib/repositories/posts_repository.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:mini_front/datasources/posts_data_source.dart';
+import 'package:mini_front/models/Post.dart';
+
+class PostsRepository {
+  final PostsDataSource dataSource;
+
+  PostsRepository(this.dataSource);
+
+  Future<List<Post>> getAllPosts() => dataSource.getAllPosts();
+  Future<Post> createPost(Post postToAdd) => dataSource.createPost(postToAdd);
+  Future<Post> updatePost(Post newPost) => dataSource.updatePost(newPost);
+}
