6caa8bb446de67a75132b30291c2b2bfcc925de2
julienEcole <julien.riviere94120@gmail.com>
2024-12-10 20:32:59 +0100

    fix : correction d'un bug mineur dans fake datasource a cause d'une erreur de const

--- lib/datasources/fake_posts_data_source.dart
+++ lib/datasources/fake_posts_data_source.dart
@@ -4,7 +4,7 @@ import 'package:mini_front/datasources/posts_data_source.dart';
 import 'package:mini_front/models/Post.dart';
 
 class FakePostsDataSource extends PostsDataSource {
-  final List<Post> _fakePosts = const [
+  final List<Post> _fakePosts = [
     Post(id: '1', title: 'Post 1', description: 'Description of Post 1'),
     Post(id: '2', title: 'Post 2', description: 'Description of Post 2'),
     Post(id: '3', title: 'Post 3', description: 'Description of Post 3'),
